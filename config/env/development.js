fc9d9f9c4a4589131c4cebb37361f7b465ba3e32
Anirudh <anirudh95@gmail.com>
2015-12-25 20:34:34 +0530

    First commit

--- config/env/development.js
+++ config/env/development.js
@@ -4,10 +4,10 @@ var defaultEnvConfig = require('./default');
 
 module.exports = {
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev',
+    uri: 'mongodb://ani:uha@ds035735.mongolab.com:35735/socialpill',
     options: {
-      user: '',
-      pass: ''
+      user: 'ani',
+      pass: 'uha'
     },
     // Enable mongoose debug mode
     debug: process.env.MONGODB_DEBUG || false
@@ -35,8 +35,8 @@ module.exports = {
     title: defaultEnvConfig.app.title + ' - Development Environment'
   },
   facebook: {
-    clientID: process.env.FACEBOOK_ID || 'APP_ID',
-    clientSecret: process.env.FACEBOOK_SECRET || 'APP_SECRET',
+    clientID: process.env.FACEBOOK_ID || '1660436024228476',
+    clientSecret: process.env.FACEBOOK_SECRET || '42bcb44cdfc984cd34a6b8b706b6e8ae',
     callbackURL: '/api/auth/facebook/callback'
   },
   twitter: {
